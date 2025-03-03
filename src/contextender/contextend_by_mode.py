5db1e04fc9fab49c1c9c391420d51e1fcc348ec7
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2025-03-03 21:40:32 +0100

    feat: add List[str] as allowed type annotation for context to item_chooser

--- src/contextender/contextend_by_mode.py
+++ src/contextender/contextend_by_mode.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, List, Union
 
 from contextender._config import ITEM_CHOOSE_TASK_MODE, SUMMARY_TASK_MODE
 from contextender.item_chooser.item_chooser import choose_item
@@ -8,7 +8,7 @@ from contextender.utils import find_context_len
 
 def contextend(
     llm: Callable,
-    text: str,  # TODO: support List[str] also (for item_chooser)
+    context: Union[str, List[str]],
     task_mode: str = SUMMARY_TASK_MODE,
     task: str = None,
     llm_context_len: int = None,
@@ -16,8 +16,8 @@ def contextend(
     if llm_context_len is None:
         llm_context_len = find_context_len(llm)
     if task_mode == SUMMARY_TASK_MODE:
-        return summarize(text, llm, llm_context_len, task)
+        return summarize(context, llm, llm_context_len, task)
     elif task_mode == ITEM_CHOOSE_TASK_MODE:
-        return choose_item(text, llm, llm_context_len, task)
+        return choose_item(context, llm, llm_context_len, task)
     else:
         pass  # TODO: raise error "unknown task_mode"
